81116d56dcf60d805cbeed9de0a720287fb4ad74
fatin99 <fatinnbs@gmail.com>
2021-08-28 15:29:35 +0800

    Solve 380

--- Medium/Design/Insert Delete GetRandom O(1)/Solution.cpp
+++ Medium/Design/Insert Delete GetRandom O(1)/Solution.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 class RandomizedSet {
 public:
+
+    unordered_map<int, bool> map;
+
     /** Initialize your data structure here. */
     RandomizedSet() {
         
@@ -10,17 +13,30 @@ public:
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        
+        if (map.find(val) == map.end()) {
+            map[val] == true;
+            return true;
+        } else {
+            return false;
+        }
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
+        if (map.find(val) == map.end()) {
+            return false;
+        } else {
+            map.erase(val);
+            return true;
+        }
         
     }
     
     /** Get a random element from the set. */
     int getRandom() {
-        
+        auto it = map.begin();
+        advance(it, rand() % map.size());
+        return it->first;
     }
 };
 
